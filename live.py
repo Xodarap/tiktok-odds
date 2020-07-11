55a7e7bf6638729f538b24218a8fb4b8f69d86cb
Ben West <Xodarap@users.noreply.github.com>
2020-07-11 14:46:50 -0700

    Update live.py

--- live.py
+++ live.py
@@ -14,8 +14,6 @@ import datetime
 import time
 import json
 
-conn=psycopg2.connect('dbname=postgres user=postgres')
-cur = conn.cursor()
 
 def load(username):
     api = TikTokApi()
@@ -30,17 +28,19 @@ def load(username):
         new_t=json.dumps(t)
         cur.execute('INSERT INTO tiktok (time,json) VALUES (%s,%s)', (t_time,new_t))
     conn.commit()
+    print('Saved')
     cur.execute('refresh materialized view tiktok.videos_all_materialized')
     cur.execute('refresh materialized view tiktok.videos_materialized')
     conn.commit()
+    print('Views Refreshed')
 
-def histogram(username):
+def histogram(df):
     plt.hist(np.log10(df['play_count']), density = True)
     plt.title('Play count distribution')
     plt.xlabel('log10(views)')
     plt.ylabel('Density')
 
-def view_like(username):
+def view_like(df):
     plt.scatter(np.log10(df['play_count']), np.log10(df['like_count']))
     plt.title('Plays vs. likes')
     plt.xlabel('log10(Plays)')
@@ -69,6 +69,8 @@ def most_recent(username):
     overtime_df = pd.DataFrame(cur.fetchall(), 
                                columns = [desc[0] for desc in cur.description])
     overtime_df['VPL'] = overtime_df['play_count'] / overtime_df['like_count']
+    overtime_df['Engagements'] = np.sum(overtime_df[['like_count', 'share_count', 'comment_count']])
+    overtime_df['EPL'] = overtime_df['play_count'] / overtime_df['Engagements']
     fig, ax = plt.subplots(1,4, sharey = 'row')
     make_overtime_plot(overtime_df, ax[0], 'Elapsed Time', 'play_count', 'like_count')
     make_overtime_plot(overtime_df, ax[1], 'Elapsed Time', 'play_count', 'share_count')
@@ -77,10 +79,48 @@ def most_recent(username):
     plt.tight_layout()
     ax[0].set_ylabel('Views')
     ax[1].set_xlabel('Minutes since publication')
-    print(f"Average VPL: {np.mean(overtime_df['VPL'])}")
+    #print(f"Average VPL: {np.mean(overtime_df['VPL'])}")
 
-username ='billnye' 
-load_only = False
+def print_stats(df):        
+    print(f"Number of videos: {len(df['VPE'])}")
+    print(f"Average VPL: {np.mean(df['VPL'])}")
+    print(f"Average Engagements: {np.mean(df['Engagements'])}")
+    print(f"Average VPE: {np.mean(df['VPE'])}")
+    print(f"Use hashtags: {np.mean(df['any_hashtags'])}")
+
+def print_extra_stats(username):
+    cur.execute('''
+                select id,
+                sum(case when hashtag_name is not null then 1 else 0 end) tags,
+                sum(case when tagged_user is not null then 1 else 0 end) users,
+                max(fetch_time - create_time) as "Elapsed Time"
+                 from tiktok.videos_all_materialized m
+                 left join tiktok.text_extra using (id)
+                 where author = (%s)
+                 group by id
+                ''', [username])        
+    df = pd.DataFrame(cur.fetchall(), 
+                               columns = [desc[0] for desc in cur.description])   
+    print(f"Average hashtags: {np.mean(df['tags'])}")
+    print(f"Average tagged users: {np.mean(df['users'])}")
+
+# =============================================================================
+# 
+# Configuration section
+# 
+# =============================================================================
+
+username ='mathematicanese' 
+load_only = True
+
+# =============================================================================
+# 
+# Main stuff
+# 
+# =============================================================================
+conn=psycopg2.connect('dbname=postgres user=postgres')
+cur = conn.cursor()
+
 if load_only:
     load(username)
 else:
@@ -90,11 +130,17 @@ else:
     res=cur.fetchall()
     colnames = [desc[0] for desc in cur.description]
     df = pd.DataFrame(res, columns = colnames)
+    df['VPL'] = df['play_count'] / df['like_count']
+    df['Engagements'] = np.sum(df[['like_count', 'share_count', 'comment_count']],
+                               axis = 1)
+    df['VPE'] = df['play_count'] / df['Engagements']
     
-    histogram(username)
+    histogram(df)
     plt.figure()
-    view_like(username)
+    view_like(df)
     most_recent(username)
+    print_stats(df)
+    print_extra_stats(username)
 
 cur.close()
 conn.close()
